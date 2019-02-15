4872d8d5b4b92024c99da6fccbc3bfde34f0ab05
woottipong523 <woottipong523@psru.ac.th>
2019-02-15 11:01:45 +0700

    Add files via upload

---
+++ src/DB.php
@@ -0,0 +1,46 @@
+<?php
+namespace woottipong523\mysqliclassdb;
+class DB{
+    private $conn = null;
+    private $last_id = null;
+    public function __construct($user, $password, $database, $host = 'localhost') {
+        try {
+            $this->conn = new mysqli( $host, $user, $password, $database );
+            $this->conn->set_charset( "utf8" );
+        } catch ( Exception $e ) {
+            die( 'Unable to connect to database' );
+        }
+    }
+
+    public function query($sql) {
+        $sep = explode(' ', strtolower(trim($sql)));
+        switch ($sep[0]) {
+            case 'select':
+                $result = $this->conn->query($sql);
+                while ( $row = $result->fetch_assoc() ) {
+                    $results[] = $row;
+                }
+                return $results;
+                break;
+            case 'insert':
+            case 'update':
+            case 'delete':
+                $query = $this->conn->query($sql);
+                $this->lastid = $this->conn->insert_id;
+                return $query;
+                break;
+            default:
+                return 'Command not found';
+        }
+    }
+    public function lastid()
+    {
+        return $this->lastid;
+    }
+
+    public function disconnect()
+    {
+        $this->conn->close();
+    }
+}
+?>
